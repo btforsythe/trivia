b48a5e8b3cd968422548e4fe91fe2f48e0147fe8
Adi Bolboaca <adi_bolb123@yahoo.com>
2014-04-28 08:32:42 +0300

    Refactored the first test

--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -10,20 +10,15 @@ import java.io.PrintStream;
 
 public class SomeTest {
 
-	@Test
-	public void true_is_true() throws Exception {
-		assertTrue(true);
-	}
-
     @Test
     public void whenGameIsCreatedSomethingHappens(){
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         PrintStream printStream = new PrintStream(stream);
         System.setOut(printStream);
+        String emptyString = "";
 
         Game game = new Game();
 
-        assertEquals("", stream.toString());
-
+        assertEquals(emptyString, stream.toString());
     }
 }
