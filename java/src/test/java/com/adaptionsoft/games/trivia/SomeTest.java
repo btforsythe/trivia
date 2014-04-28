01e720299f6522374b5044c7458450f2e9e4615f
Adi Bolboaca <adi_bolb123@yahoo.com>
2014-04-28 08:31:27 +0300

    When a Game is created the output written is empty string

--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -1,12 +1,29 @@
 package com.adaptionsoft.games.trivia;
 
 import static org.junit.Assert.*;
+
+import com.adaptionsoft.games.uglytrivia.Game;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class SomeTest {
 
 	@Test
 	public void true_is_true() throws Exception {
-		assertTrue(false);
+		assertTrue(true);
 	}
+
+    @Test
+    public void whenGameIsCreatedSomethingHappens(){
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(stream);
+        System.setOut(printStream);
+
+        Game game = new Game();
+
+        assertEquals("", stream.toString());
+
+    }
 }
