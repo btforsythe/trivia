c2944fb3718e0aade7acba564491443fb76860b6
Adi Bolboaca <adi_bolb123@yahoo.com>
2014-04-30 20:12:36 +0300

    Added some tests for roll

--- java/src/test/java/com/adaptionsoft/games/trivia/GameTests.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/GameTests.java
@@ -75,4 +75,119 @@ public class GameTests {
                 "The category is Science\n" +
                 "Science Question 0\n", stream.toString());
     }
+
+    @Test
+    public void Smth(){
+        game.add("SomePlayer");
+        ByteArrayOutputStream stream = getConsoleOutput();
+
+        game.roll(1);
+
+        assertEquals("SomePlayer is the current player\n" +
+                "They have rolled a 1\n" +
+                "SomePlayer's new location is 1\n" +
+                "The category is Science\n" +
+                "Science Question 0\n", stream.toString());
+    }
+
+    @Test
+    public void Smth2(){
+        game.add("SomePlayer");
+        ByteArrayOutputStream stream = getConsoleOutput();
+
+        game.roll(2);
+
+        assertEquals("SomePlayer is the current player\n" +
+                "They have rolled a 2\n" +
+                "SomePlayer's new location is 2\n" +
+                "The category is Sports\n" +
+                "Sports Question 0\n", stream.toString());
+    }
+
+    @Test
+    public void Smth3(){
+        game.add("SomePlayer");
+        ByteArrayOutputStream stream = getConsoleOutput();
+
+        game.roll(3);
+
+        assertEquals("SomePlayer is the current player\n" +
+                "They have rolled a 3\n" +
+                "SomePlayer's new location is 3\n" +
+                "The category is Rock\n" +
+                "Rock Question 0\n", stream.toString());
+    }
+
+    @Test
+    public void Smth4(){
+        game.add("SomePlayer");
+        ByteArrayOutputStream stream = getConsoleOutput();
+
+        game.roll(4);
+
+        assertEquals("SomePlayer is the current player\n" +
+                "They have rolled a 4\n" +
+                "SomePlayer's new location is 4\n" +
+                "The category is Pop\n" +
+                "Pop Question 0\n", stream.toString());
+    }
+
+
+    @Test
+    public void Smth5(){
+        game.add("SomePlayer");
+        ByteArrayOutputStream stream = getConsoleOutput();
+
+        game.roll(5);
+
+        assertEquals("SomePlayer is the current player\n" +
+                "They have rolled a 5\n" +
+                "SomePlayer's new location is 5\n" +
+                "The category is Science\n" +
+                "Science Question 0\n", stream.toString());
+    }
+
+    @Test
+    public void Smth6(){
+        game.add("SomePlayer");
+        ByteArrayOutputStream stream = getConsoleOutput();
+
+        game.roll(6);
+
+        assertEquals("SomePlayer is the current player\n" +
+                "They have rolled a 6\n" +
+                "SomePlayer's new location is 6\n" +
+                "The category is Sports\n" +
+                "Sports Question 0\n", stream.toString());
+    }
+
+
+    @Test
+    public void Smth7(){
+        game.add("SomePlayer");
+        ByteArrayOutputStream stream = getConsoleOutput();
+
+        game.roll(7);
+
+        assertEquals("SomePlayer is the current player\n" +
+                "They have rolled a 7\n" +
+                "SomePlayer's new location is 7\n" +
+                "The category is Rock\n" +
+                "Rock Question 0\n", stream.toString());
+    }
+
+
+    @Test
+    public void Smth13(){
+        game.add("SomePlayer");
+        ByteArrayOutputStream stream = getConsoleOutput();
+
+        game.roll(13);
+
+        assertEquals("SomePlayer is the current player\n" +
+                "They have rolled a 13\n" +
+                "SomePlayer's new location is 1\n" +
+                "The category is Science\n" +
+                "Science Question 0\n", stream.toString());
+    }
 }
